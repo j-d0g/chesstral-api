736b0ec245986f1e9625d153bc765041f7c847a3
Jordan Tran <jordan.tran@student.manchester.ac.uk>
2024-04-19 21:36:24 +0100

    refactor app logic to reduce redundancies

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ import logging
 from repository.chatgpt import ChatGPT
 from repository.llama import Llama
 from repository.mistral import Mistral
-from service.stockfish import get_stockfish_move
-from service.engine import get_llm_move
+from service.stockfish_service import get_stockfish_move
+from service.llm_service import get_llm_move
 
 from dotenv import load_dotenv
 import os
@@ -23,27 +23,23 @@ def get_computer_move():
     data = request.get_json()
     fen = data['fen']
     engine_name = data['engine']
+    load_dotenv()
 
     pprint(f'Engine name: {engine_name}')
 
-    load_dotenv()
     if 'stockfish' in engine_name:
-        move_json = get_stockfish_move(fen, os.getenv('STOCKFISH_PATH'))
+        return get_stockfish_move(fen, os.getenv('STOCKFISH_PATH'))
     elif 'gpt' in engine_name:
-        print("APP->GPT")
-        gpt = ChatGPT(os.getenv('GPT_API_KEY'))
-        move_json = get_llm_move(fen, gpt, engine_name)
+        llm = ChatGPT(os.getenv('GPT_API_KEY'))
     elif 'meta' in engine_name:
-        llama = Llama(os.getenv('REPLICATE_API_KEY'))
-        move_json = get_llm_move(fen, llama, engine_name)
+        llm = Llama(os.getenv('REPLICATE_API_KEY'))
     elif 'mistral' or 'mixtral' in engine_name:
-        mistral = Mistral(os.getenv('MISTRAL_API_KEY'))
-        move_json = get_llm_move(fen, mistral, engine_name)
+        llm = Mistral(os.getenv('MISTRAL_API_KEY'))
     else:
-        logging.error(f'Invalid engine name: {engine_name}')
-        move_json = None
+        logging.error(f'Engine not yet supported: {engine_name}')
+        return None
 
-    return jsonify(move_json)
+    return jsonify(get_llm_move(fen, llm, engine_name))
 
 
 if __name__ == '__main__':
