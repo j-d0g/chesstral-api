7910c68e8f66b22bd71abde3f6af25b586b4792b
Jordan Tran <jordan.tran@student.manchester.ac.uk>
2024-05-05 00:23:06 +0100

    clean again

--- service/llm_service.py
+++ service/llm_service.py
@@ -13,13 +13,13 @@ def get_llm_move(pgn_moves: list[str], fen: str, llm: BaseLLM, model_name: str, 
                  max_retries: int = 10, reset_cycle: int = 5) -> dict:
     """
     Generates a chess move using the Mistral API.
-    :param reset_cycle: number of retries before resetting the error messages
-    :param feature_flags: list of booleans corresponding to what features to include in the prompt
     :param pgn_moves: list of moves in SAN string format
     :param fen: string representing the current board state
-    :param llm: Language model instance
+    :param feature_flags: list of booleans corresponding to what features to include in the prompt
+    :param llm: selected llm chess-engine instance
     :param model_name: Name of the language model to use (optional)
     :param max_retries: Maximum number of retries before giving up (default: 15)
+    :param reset_cycle: number of retries before resetting the error messages
     :return: JSON object containing the move and thoughts, or an error message
     """
 
@@ -33,7 +33,7 @@ def get_llm_move(pgn_moves: list[str], fen: str, llm: BaseLLM, model_name: str, 
     reprompt_counter = defaultdict(int)
     prompt: str = user_chess_prompt(board, pgn_moves, feature_flags)
 
-    # HANDLE GENERATION & RETRIES
+    # Reprompt Loop
     for retry in range(max_retries):
         if (retry + 1) % reset_cycle == 0:
             [llm.pop_message() for _ in range(retry % reset_cycle)]
@@ -41,10 +41,10 @@ def get_llm_move(pgn_moves: list[str], fen: str, llm: BaseLLM, model_name: str, 
         output: str = llm.grab_text(prompt, model_name=model_name)
         response_json: dict = validate_json(output)
 
-        if 'error' not in response_json:
+        if 'reprompt' not in response_json:
             response_json: dict = validate_move(board, response_json)
 
-        if 'error' not in response_json:
+        if 'reprompt' not in response_json:
             benchmarks: dict = dump_data(
                 response_json,
                 feature_flags,
@@ -56,7 +56,7 @@ def get_llm_move(pgn_moves: list[str], fen: str, llm: BaseLLM, model_name: str, 
             [llm.pop_message() for _ in range(retry)]
             return benchmarks
 
-        prompt = f"{response_json['error']}. Previous prompt: '''{user_chess_prompt(board, pgn_moves, feature_flags)}'''"
+        prompt = f"{response_json['reprompt']}. Previous prompt: '''{user_chess_prompt(board, pgn_moves, feature_flags)}'''"
         increment_reprompt(response_json['error'], reprompt_counter)
 
     raise ValueError("Max retries exceeded. Unable to generate a valid response.")
@@ -65,8 +65,8 @@ def get_llm_move(pgn_moves: list[str], fen: str, llm: BaseLLM, model_name: str, 
 def upgrade_model(model_name: str) -> str:
     """
     A solution to consistently bad re-prompts: upgrading to larger models after a certain number of retries.
-    :param model_name:
-    :return:
+    :param model_name: current model name
+    :return: next model name
     """
     if "mistral" in model_name or "mixtral" in model_name:
         models = {
@@ -90,9 +90,9 @@ def upgrade_model(model_name: str) -> str:
 def upgrade(models, model_name):
     """
     Helper function for upgrade_model
-    :param models:
-    :param model_name:
-    :return:
+    :param models: dict of models
+    :param model_name: current model name
+    :return: next model name
     """
     inverted_models = {v: k for k, v in models.items()}
     current_model = inverted_models[model_name]
