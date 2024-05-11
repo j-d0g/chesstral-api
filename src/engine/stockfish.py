0bdf018f7c8191cdf3107f29c27816dde8c9e7a2
Jordan Tran <jordan.tran@student.manchester.ac.uk>
2024-05-11 21:08:46 +0100

    add stockfish evaluation score

--- src/engine/stockfish.py
+++ src/engine/stockfish.py
@@ -1,7 +1,9 @@
 import chess.engine
+import os
 from flask import jsonify
 from dotenv import load_dotenv
-import os
+
+from util.chess_features import to_san
 
 load_dotenv()
 
@@ -13,18 +15,36 @@ class Stockfish:
 
     def get_stockfish_move(self, fen):
         board = chess.Board(fen)
-
-        move = self.engine.play(board, chess.engine.Limit(time=0.1)).move
+        result = self.engine.play(board, chess.engine.Limit(time=0.1))
+        move = result.move
         commentary = f"Stockfish plays {move}"
-        return jsonify({'move': str(move), 'commentary': commentary})
+        return {'move': str(move), 'commentary': commentary}
 
-    def get_stockfish_eval(self, fen):
+    def get_stockfish_evaluation(self, fen):
         board = chess.Board(fen)
-
         info = self.engine.analyse(board, chess.engine.Limit(time=0.1))
-        return jsonify({'evaluation': str(info['score'])})
+        score = info['score'].relative.score(mate_score=10000)  # mate_score optional for clarity on mate situations
+        # Convert score to a more readable format: positive for white's advantage, negative for black's
+        if score is not None:
+            readable_score = score / 100.0  # converting centi pawns to pawns
+        else:
+            readable_score = "Mate imminent"
+        return {'evaluation': readable_score}
 
 
-# test get_stockfish_eval
-chess.board = chess.Board()
-stockfish_path = 'stockfish'
+# Example Usage:
+
+# Create an instance of the Stockfish class
+stockfish = Stockfish()
+# Get the best move and evaluation for the initial position
+board = chess.Board()
+fen = board.fen()
+evaluation_response = stockfish.get_stockfish_evaluation(fen)
+print(evaluation_response)
+
+move_response = stockfish.get_stockfish_move(fen)
+board.push(chess.Move.from_uci(move_response['move']))
+fen = board.fen()
+evaluation_response = stockfish.get_stockfish_evaluation(fen)
+print(move_response['move'])
+print(evaluation_response)
